17a304eacabccb7d5ae3ef6978951c0427bacd58
Yudhistira Erlandinata <yudhistira.e@tokopedia.com>
2019-09-09 13:52:24 +0700

    Add challenge1_senin.py

---
+++ challenge1_senin.py
@@ -0,0 +1,26 @@
+import turtle
+import math
+
+r1 = float(input())
+r2 = float(input())
+d = float(input())
+ 
+# mulai gambar r2
+turtle.circle(r2)
+turtle.forward(d)
+ 
+# math stuff
+sudut = math.atan(d/(r1 - r2))
+sudut = 360 - 2*math.degrees(sudut)
+ 
+# gambar lingkaran r1
+turtle.circle(r1)
+ 
+# gerak ke posisi singgung
+turtle.penup()
+turtle.circle(r1, sudut)
+turtle.pendown()
+ 
+# buat garis singgung
+turtle.forward(d)
+turtle.exitonclick()
