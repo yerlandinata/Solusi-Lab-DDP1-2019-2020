d3a005e53b15a8323dc1a4a45f4feb886961b3f4
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2019-09-09 23:50:57 +0700

    fix latihan 4 senin

--- latihan4_senin.py
+++ latihan4_senin.py
@@ -7,13 +7,33 @@ Created on Sun Sep  8 14:58:38 2019
 
 import turtle
 
-turtle.color('blue')
-turtle.begin_fill()
-turtle.circle(100)
-turtle.end_fill()
+warna_kesukaan = input('Apa warna kesukaan mu? ')
+
+if warna_kesukaan == 'merah':
+    turtle.color('red')
+elif warna_kesukaan == 'hijau':
+    turtle.color('green')
+elif warna_kesukaan == 'kuning':
+    turtle.color('yellow')
+    
+if warna_kesukaan != 'merah' and warna_kesukaan != 'hijau' and warna_kesukaan != 'kuning':
+    turtle.forward(100)
+
+    turtle.right(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(100)
+    
+    turtle.right(90)
+    turtle.forward(100)
+else:
+    turtle.begin_fill()
+    turtle.circle(100)
+    turtle.end_fill()
 
 
-
+turtle.hideturtle()
 
 turtle.exitonclick()
 turtle.mainloop()
